75350d5df5ebea2f3d5c410bb7b150516b4b2b59
Abhilash Reddy Kallepu <HadiHatinoglu@Hadis-iMac.local>
2013-12-05 13:44:31 +0530

    Initial Commit

---
+++ btnStatus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  btnStatus
+//
+//  Created by Hadi Hatinoglu on 05/12/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ btnStatus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  btnStatus
+//
+//  Created by Hadi Hatinoglu on 05/12/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
